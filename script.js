553cf762829d86e703c664e84120fb7bbb44bb55
wavymasch <83346160+wavymasch@users.noreply.github.com>
2024-10-31 01:03:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const apiKey = 'YOUR_ECOWITT_API_KEY';
 const stationId = 'YOUR_STATION_ID';
 const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-const apiUrl = `${proxyUrl}https://api.ecowitt.net/api/v3/device/real_time?imei=&call_back=all&temp_unitid=1&pressure_unitid=3&wind_speed_unitid=6&rainfall_unitid=12&solar_irradiance_unitid=16&application_key=0D8C7C8B278C5E9469CD336E3B7F85CE&api_key=753bd0bd-4565-47d0-9873-d6b3dbef2428&mac=F0:08:D1:07:41:B8`;
+const apiUrl = `https://api.ecowitt.net/api/v3/device/real_time?imei=&call_back=all&temp_unitid=1&pressure_unitid=3&wind_speed_unitid=6&rainfall_unitid=12&solar_irradiance_unitid=16&application_key=0D8C7C8B278C5E9469CD336E3B7F85CE&api_key=753bd0bd-4565-47d0-9873-d6b3dbef2428&mac=F0:08:D1:07:41:B8`;
 
 // Function to fetch and update data
 async function fetchWeatherData() {
